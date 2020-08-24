2c7a22cadbf48dc199394f4261ec6fe21f03d2f0
Yavor Keskinev <yavor_keskinev@abv.bg>
2020-08-24 21:35:04 +0300

    second commit

---
+++ dist/index.d.ts
@@ -0,0 +1,10 @@
+export declare enum Roles {
+    beableadmin = "beableadmin",
+    districtadmin = "districtadmin",
+    contentloader = "contentloader",
+    itadmin = "itadmin"
+}
+export declare class CreateGameMessage {
+    action: string;
+    name: string;
+}
---
+++ dist/index.js
@@ -0,0 +1,14 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.CreateGameMessage = exports.Roles = void 0;
+var Roles;
+(function (Roles) {
+    Roles["beableadmin"] = "beableadmin";
+    Roles["districtadmin"] = "districtadmin";
+    Roles["contentloader"] = "contentloader";
+    Roles["itadmin"] = "itadmin";
+})(Roles = exports.Roles || (exports.Roles = {}));
+class CreateGameMessage {
+}
+exports.CreateGameMessage = CreateGameMessage;
+//# sourceMappingURL=index.js.map
