59765dfba6d1c8ff7700b2af60f3bfb4ca505431
Yavor Keskinev <yavor_keskinev@abv.bg>
2020-08-24 21:45:42 +0300

    version reset

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,3 @@
-export enum Roles {
-  beableadmin = 'beableadmin',
-  districtadmin = 'districtadmin',
-  contentloader = 'contentloader',
-  itadmin = 'itadmin'
-}
-
 export class CreateGameMessage {
   action: string;
   name: string;
